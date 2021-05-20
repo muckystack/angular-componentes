68a8ef4fa3423c6df4a9f4cb790179ba75eb1e8f
muckystack <muckystack@gmail.com>
2021-05-19 20:45:36 -0500

    FEAT: Formularios reactivos con validaciones explicadas

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,17 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { LoginFormErroresComponent } from './login-form-errores/login-form-errores.component';
+import { ErrorTailorModule } from '@ngneat/error-tailor';
 
 
 
 @NgModule({
-  declarations: [LoginFormComponent],
+  declarations: [LoginFormComponent, LoginFormErroresComponent],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    ErrorTailorModule.forRoot({
+      errors: {
+        useValue: {
+          required: 'El campo es requerido',
+          minlength: ({ requiredLength, actualLength }) =>
+            `Expect ${requiredLength} but got ${actualLength}`,
+          invalidAddress: (error) => `Address isn't valid`,
+        },
+      },
+    }),
   ],
-  exports: [
-    LoginFormComponent
-  ]
+  exports: [LoginFormComponent, LoginFormErroresComponent],
 })
-export class ComponentsModule { }
+export class ComponentsModule {}
--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -4,7 +4,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.css'],
+  styleUrls: [],
 })
 export class LoginFormComponent implements OnInit {
   public form: FormGroup;
